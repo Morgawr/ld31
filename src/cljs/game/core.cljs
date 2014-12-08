8513da363d5c034706367fbb356f88c1f8731627
Morgawr <morgawr@gmail.com>
2014-12-08 23:04:22 +0000

    updated to latest version of novelette engine to fix bug

--- src/cljs/game/core.cljs
+++ src/cljs/game/core.cljs
@@ -1,8 +1,6 @@
 (ns game.core
   (:require [clojure.browser.repl :as repl]
             [goog.dom :as dom]
-            [goog.events :as events]
-            [goog.events.EventType :as event-type]
             novelette.screens.loadingscreen
             novelette.screens.storyscreen
             [novelette.input :as input]
@@ -37,12 +35,6 @@
   (into novelette.screens.storyscreen/BASE-STATE
         {:scrollfront (:body script/start)}))
 
-;(defn ^:export connect
-;  []
-;  (.log js/console "Starting local connection...")
-;  (repl/connect "http://localhost:9000/repl")
-;  (.log js/console "...connected"))
-
 (defn ^:export init []
   (let [document (dom/getDocument)
         canvas (dom/getElement "surface")
@@ -51,6 +43,6 @@
                                                       (novelette.screens.storyscreen/init  ctx  canvas
                                                        start-game))
         state (screen/State. [loading] 0 ctx canvas)]
-    (events/listen js/window event-type/MOUSEMOVE input/mouse-move-listener)
-    (events/listen js/window event-type/CLICK input/mouse-click-listener)
+    (.log js/console (pr-str @input/BOUNDING-BOX))
+    (input/init canvas)
     (screen/main-game-loop state)))
